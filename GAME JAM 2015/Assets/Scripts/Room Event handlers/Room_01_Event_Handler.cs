4528fbdae1cd33db6c02dc210450412a79e75120
Sonachi <red_515@hotmail.com>
2015-01-24 12:25:15 -0500

    FO REAL!
    ...fixed scripting error...

--- GAME JAM 2015/Assets/Scripts/Room Event handlers/Room_01_Event_Handler.cs
+++ GAME JAM 2015/Assets/Scripts/Room Event handlers/Room_01_Event_Handler.cs
@@ -9,14 +9,15 @@ public class Room_01_Event_Handler : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        flags = gameObject.transform.Find("First Person Duck Controller").GetComponent<GLOBAL_FLAGS>();
+        flags = GameObject.Find("First Person Duck Controller").GetComponent<GLOBAL_FLAGS>();
 
-        if (flags.IS_THE_NOTE_MISSING)
-            Note.SetActive(false);
+            if (flags.IS_THE_NOTE_MISSING)
+                Note.SetActive(false);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+
 	}
 }

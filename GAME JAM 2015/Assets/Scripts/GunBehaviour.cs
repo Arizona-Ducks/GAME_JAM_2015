c166288a4521638ad7464bdfbfeb17d7499ab1cd
Note911 <kees.m.vermeulen@gmail.com>
2015-01-24 14:09:00 -0500

    Added Gun Model and animation controller
    Gun model is in bitches!

---
+++ GAME JAM 2015/Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunBehaviour : MonoBehaviour 
+{
+
+    protected Animator animator;
+	// Use this for initialization
+	void Start () 
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("Shoot", false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1))
+            animator.SetBool("Shoot", true);
+        else
+            animator.SetBool("Shoot", false);
+	}
+}

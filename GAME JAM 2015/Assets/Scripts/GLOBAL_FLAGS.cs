691c7a0e8977e5a32c845705be53889c7da79f7d
Sonachi <red_515@hotmail.com>
2015-01-24 12:15:04 -0500

    and some other bullshit...

---
+++ GAME JAM 2015/Assets/Scripts/GLOBAL_FLAGS.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GLOBAL_FLAGS : MonoBehaviour
+{
+    //All flag / global variables will be store in this script attached to the player.
+    
+    //GLOBAL
+    public bool HAS_THE_GEM = false;
+    
+    //Room_01 (firstRoom)
+    public bool IS_THE_NOTE_MISSING = false;
+
+    //Room_02
+    public int NUMBER_OF_ROOM_02_BUTTONS_PRESSED = 0;
+}
---
+++ GAME JAM 2015/Assets/Scripts/Room Event handlers/Room_01_Event_Handler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room_01_Event_Handler : MonoBehaviour {
+
+    public GameObject Note;
+    GLOBAL_FLAGS flags;
+
+	// Use this for initialization
+	void Start () 
+    {
+        flags = gameObject.transform.Find("First Person Duck Controller").GetComponent<GLOBAL_FLAGS>();
+
+        if (flags.IS_THE_NOTE_MISSING)
+            Note.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

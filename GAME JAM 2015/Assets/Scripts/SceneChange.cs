084ec45863cb84b55eee507e8384c65cf3a82ae1
Stefurak <stfs0047@humbermail.ca>
2015-01-24 04:20:37 -0500

    Scene Change Script/Collision Prefab
    ^^
    Got the scene change script started and going from the first room to the
    second room and back.
    Added the collision box as a prefab.

---
+++ GAME JAM 2015/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+    GameObject player;
+    Vector3 playerPos;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update(){}
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player"){
+        Debug.Log("error");
+            if (Application.loadedLevelName == "FirstRoom" && this.name == "To Room_02")
+            {
+                playerPos.x = 9;
+                playerPos.y = 2;
+                playerPos.z = -23;
+                Application.LoadLevel("Room_02");
+            }
+            else if(Application.loadedLevelName == "Room_02" && this.name == "To FirstRoom_00"){
+                playerPos.x = 6;
+                playerPos.y = 2;
+                playerPos.z = 4;
+                Application.LoadLevel("FirstRoom");
+            }
+            else if (Application.loadedLevelName == "Room_02" && this.name == "To Room_03") {
+            
+            }
+            else if (Application.loadedLevelName == "Room_03" && this.name == "To FirstRoom_01")
+            {
+
+            }
+        }
+    }
+}

70b5898e7f2bedb02594b2e69ec8f7016febb810
Sonachi <red_515@hotmail.com>
2015-01-24 13:56:08 -0500

    Smokey Smokes
    Leap of faith into the purple haze~~!

--- GAME JAM 2015/Assets/Scripts/Room Event handlers/Room_01_Event_Handler.cs
+++ GAME JAM 2015/Assets/Scripts/Room Event handlers/Room_01_Event_Handler.cs
@@ -11,8 +11,23 @@ public class Room_01_Event_Handler : MonoBehaviour {
     {
         flags = GameObject.Find("First Person Duck Controller").GetComponent<GLOBAL_FLAGS>();
 
-            if (flags.IS_THE_NOTE_MISSING)
-                Note.SetActive(false);
+        if (flags.IS_THE_NOTE_MISSING)
+            Note.SetActive(false);
+
+        if (flags.IS_ARRIVING_FROM_ROOM_03_TO_FIRST_ROOM)
+        {
+            Transform player = GameObject.Find("First Person Duck Controller").transform;
+            Transform spawn = GameObject.Find("FromRoom_03").transform;
+
+            Debug.Log(spawn.position);
+            Debug.Log(player.position);
+            Debug.Log(spawn.position - player.position);
+
+            player.Translate(spawn.position - player.position, Space.World);
+            flags.IS_ARRIVING_FROM_ROOM_03_TO_FIRST_ROOM = false;
+
+            Debug.Log(player.position);
+        }
 	}
 	
 	// Update is called once per frame
--- GAME JAM 2015/Assets/Scripts/SceneChange.cs
+++ GAME JAM 2015/Assets/Scripts/SceneChange.cs
@@ -3,8 +3,24 @@ using System.Collections;
 
 public class SceneChange : MonoBehaviour {
 
+    GLOBAL_FLAGS flags;
+
+    // Use this for initialization
+    void Start()
+    {
+        flags = GameObject.Find("First Person Duck Controller").GetComponent<GLOBAL_FLAGS>();
+    }
+
     void OnTriggerEnter(Collider col)
     {
+        if (Application.loadedLevelName == "Room_03" && col.gameObject.tag == "Player" && this.name.Substring(3) == "FirstRoom")
+        {
+            flags.IS_THE_NOTE_MISSING = true;
+            flags.IS_ARRIVING_FROM_ROOM_03_TO_FIRST_ROOM = true;
+
+            Application.LoadLevel(this.name.Substring(3));
+        }
+
         if(col.gameObject.tag == "Player")
             Application.LoadLevel(this.name.Substring(3));
     }

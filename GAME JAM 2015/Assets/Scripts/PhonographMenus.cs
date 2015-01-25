88d82c4a4dcf3a890682a0b284cb524d0844a8d8
Note911 <kees.m.vermeulen@gmail.com>
2015-01-25 14:21:56 -0500

    fixed music in menus

---
+++ GAME JAM 2015/Assets/Scripts/PhonographMenus.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhonographMenus : MonoBehaviour
+{
+
+    AudioSource recordNorm, recordRev;
+    public bool isRev;
+    public bool isNorm;
+
+    float currentPosition;
+
+    // Use this for initalization
+    void Start()
+    {
+        currentPosition = 0;
+        recordNorm = gameObject.transform.FindChild("RecordNorm").transform.GetComponent<AudioSource>();
+        recordRev = gameObject.transform.FindChild("RecordRev").transform.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (isRev && !recordRev.isPlaying)
+        {
+            recordRev.Play();
+            recordRev.time = currentPosition;
+            isNorm = false;
+        }
+
+        if (isNorm && !recordNorm.isPlaying)
+        {
+            recordNorm.Play();
+            recordNorm.time = currentPosition;
+            isRev = false;
+        }
+    }
+}

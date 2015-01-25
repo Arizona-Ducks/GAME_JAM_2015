c90a49e1136d29563936ad076e782374d6389cfd
Stefurak <stfs0047@humbermail.ca>
2015-01-24 19:50:34 -0500

    Vert/Horz platforms & Room05
    Got the platforms working, they aren't conflicting so add as many as you
    want, just watch the naming conventions if you mess with them(v/vert and
    h/horiz)
    Room 05 is the base with one room with a vertical platform

---
+++ GAME JAM 2015/Assets/Scripts/HorizontalMovingPlatform.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalMovingPlatform : MonoBehaviour {
+    Transform platform;
+    GameObject leftLimit;
+    GameObject rightLimit;
+    Vector3 hspeed;
+    bool stopped;
+    bool left = false;
+    Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        platform = gameObject.transform.FindChild("PlatformH");
+        leftLimit = gameObject.transform.FindChild("LeftPoint").gameObject;
+        rightLimit = gameObject.transform.FindChild("RightPoint").gameObject;
+        hspeed = new Vector3(0, 0, 1);
+        stopped = false;
+        player = GameObject.Find("First Person Duck Controller").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray playerLookRay = new Ray(player.FindChild("Main Camera").position, player.FindChild("Main Camera").forward);
+            RaycastHit hitInfo;
+
+            //checks for raycast hit from player camera
+            if (platform.gameObject.collider.Raycast(playerLookRay, out hitInfo, 10))
+            {
+                //stalls movement if moving
+                if (!stopped)
+                {
+                    Frozen();
+                }
+                //starts movement if frozen
+                else if (stopped)
+                {
+                    Thawed();
+                }
+            }
+        }
+
+        if (!stopped)
+        {
+            platform.transform.Translate(Moving(hspeed) / 30.0f);
+        }
+    }
+
+    Vector3 Moving(Vector3 hspeed)
+    {
+        if (platform.transform.localPosition.z > leftLimit.transform.localPosition.z && left)
+        {
+            left = false;
+        }
+        else if (platform.transform.localPosition.z < rightLimit.transform.localPosition.z && !left)
+        {
+            left = true;
+        }
+        Debug.Log(left);
+
+        if (left)
+            return hspeed;
+        else
+            return -hspeed;
+    }
+
+    void Frozen()
+    {
+        stopped = true;
+    }
+
+    void Thawed()
+    {
+        stopped = false;
+    }
+}
+
---
+++ GAME JAM 2015/Assets/Scripts/VerticalMovingPlatform.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalMovingPlatform : MonoBehaviour {
+    Transform platform;
+    GameObject topLimit;
+    GameObject bottomLimit;
+    Vector3 vspeed;
+    bool stopped;
+    bool down = false;
+
+    Transform player;
+
+	// Use this for initialization
+	void Start () {
+        platform = gameObject.transform.FindChild("PlatformV");
+        topLimit = gameObject.transform.FindChild("TopPoint").gameObject;
+        bottomLimit = gameObject.transform.FindChild("BottomPoint").gameObject;
+        vspeed = new Vector3(0, -1, 0);
+        stopped = false;
+        player = GameObject.Find("First Person Duck Controller").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray playerLookRay = new Ray(player.FindChild("Main Camera").position, player.FindChild("Main Camera").forward);
+            RaycastHit hitInfo;
+
+            //checks for raycast hit from player camera
+            if (platform.gameObject.collider.Raycast(playerLookRay, out hitInfo, 10))
+            {
+                //stalls movement if moving
+                if (!stopped)
+                {
+                    Frozen();
+                }
+                //starts movement if frozen
+                else if (stopped)
+                {
+                    Thawed();
+                }
+            }
+        }
+        if (!stopped)
+        {
+            platform.transform.Translate(Moving(vspeed) / 15.0f);
+        }
+	}
+
+    Vector3 Moving(Vector3 vspeed)
+    {
+        if (platform.transform.localPosition.y > topLimit.transform.localPosition.y && !down)
+        {
+            down = true;
+        }
+        else if (platform.transform.localPosition.y < bottomLimit.transform.localPosition.y && down)
+        {
+            down = false;
+        }
+        Debug.Log(down);
+
+        if (down)
+            return vspeed;
+        else
+            return -vspeed;
+    }
+    void Frozen()
+    {
+        stopped = true;
+    }
+    void Thawed()
+    {
+        stopped = false;
+    }
+}

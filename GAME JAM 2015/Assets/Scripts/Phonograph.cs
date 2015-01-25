1021813f240c120268f3d43ffefc450cb48516a2
hanzolo95 <zlnl0037@humbermail.ca>
2015-01-25 00:07:56 -0500

    Phonograh room works!

---
+++ GAME JAM 2015/Assets/Scripts/Phonograph.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Phonograph : MonoBehaviour
+{
+
+    AudioSource recordNorm, recordRev;
+    bool isRev;
+    bool isNorm;
+
+
+
+    // Use this for initalization
+    void Start()
+    {
+
+        recordNorm = gameObject.transform.FindChild("RecordNorm").transform.GetComponent<AudioSource>();
+        recordRev = gameObject.transform.FindChild("RecordRev").transform.GetComponent<AudioSource>();
+
+        isRev = true;
+        isNorm = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRev && !recordRev.isPlaying)
+        {
+            recordRev.Play();
+            isNorm = false;
+        }
+
+        if (isNorm && !recordNorm.isPlaying)
+        {
+            recordNorm.Play();
+            isRev = false;
+        }
+    }
+}

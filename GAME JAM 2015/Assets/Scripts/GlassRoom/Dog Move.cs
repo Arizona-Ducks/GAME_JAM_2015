e75bb51db19f4fa8672b3f6d69dcc341853c708d
Torakuma <chamorro.e@hotmail.com>
2015-01-25 11:22:16 +0900

    Glass room update
    Glass room, duck hunt, hallway, prefabs, materials, scripts

---
+++ GAME JAM 2015/Assets/Scripts/GlassRoom/Dog Move.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogMove : MonoBehaviour {
+
+    public  Vector3 velocity = new Vector3(100.0f,0.0f,0.0f);
+    public Vector3 acceleration= new Vector3(5.0f,0.0f,0.0f);
+    public Vector3 position;
+	// Use this for initialization
+	
+        void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Button Trigger")
+        {
+            velocity *= -1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float timestep = 1;
+        ///update velocity and do the wall collisions here
+
+
+        position = new Vector3(0.5f * acceleration.x * timestep * timestep, -9.8f, 0.5f * acceleration.z * timestep * timestep);
+
+
+
+        rigidbody.position += velocity;
+        
+	}
+}

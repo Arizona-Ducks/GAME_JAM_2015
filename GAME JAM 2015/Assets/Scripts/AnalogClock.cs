8c50c6676bd824534290f1f55cb3e643568d5dfb
Sonachi <red_515@hotmail.com>
2015-01-25 13:21:58 -0500

    Room 4 updates
    TIMELORD MCQUACK!

--- GAME JAM 2015/Assets/Scripts/AnalogClock.cs
+++ GAME JAM 2015/Assets/Scripts/AnalogClock.cs
@@ -8,12 +8,59 @@ public class AnalogClock : MonoBehaviour
     public Transform minutesPivot;
     public Transform hoursPivot;
 
+    public bool StartsInReverse = false;
+
+    bool timeRunningFoward;
+    public int timeSpeed = 1;
+    DateTime currentTime;
+    Transform player;
+
+    void Start()
+    {
+        player = GameObject.Find("First Person Duck Controller").transform;
+        currentTime = DateTime.Now;
+        timeRunningFoward = !StartsInReverse;
+    }
+
 	void Update () 
     {
-        DateTime time = DateTime.Now;
-        float seconds = (float)time.Second;
-        float minutes = (float)time.Minute;
-        float hours = (float)time.Hour;
+        if (player.transform.FindChild("Main Camera").transform.FindChild("TimeGun").GetComponent<GunBehaviour>().FIRING_FORWARD)
+        {
+            Ray gunLookRay = new Ray(player.FindChild("Main Camera").transform.FindChild("TimeGun").position, player.FindChild("Main Camera").transform.FindChild("TimeGun").right);
+            RaycastHit hitInfo;
+
+            //checks for raycast hit from gun camera
+            if (gameObject.transform.FindChild("Clock-Face").gameObject.collider.Raycast(gunLookRay, out hitInfo, 30))
+            {
+                ChangeTimeDirectionToFoward();
+            }
+        }
+        else if (player.transform.FindChild("Main Camera").transform.FindChild("TimeGun").GetComponent<GunBehaviour>().FIRING_BACKWARD)
+        {
+            Ray gunLookRay = new Ray(player.FindChild("Main Camera").transform.FindChild("TimeGun").position, player.FindChild("Main Camera").transform.FindChild("TimeGun").right);
+            RaycastHit hitInfo;
+
+            //checks for raycast hit from gun camera
+            if (gameObject.transform.FindChild("Clock-Face").gameObject.collider.Raycast(gunLookRay, out hitInfo, 30))
+            {
+                ChangeTimeDirectionToReverse();
+            }
+        }
+
+
+        if (timeRunningFoward)
+        {
+            currentTime = currentTime.AddSeconds(Time.fixedDeltaTime * timeSpeed);
+        }
+        else
+        {
+            currentTime = currentTime.AddSeconds(Time.fixedDeltaTime * -timeSpeed);
+            Debug.Log(currentTime);
+        }
+
+        float seconds = (float)currentTime.Second;
+        float minutes = (float)currentTime.Minute;
+        float hours = (float)currentTime.Hour;
 
         float angleSeconds = -360 * (seconds / 60);
         float angleMinutes = -360 * (minutes / 60);
@@ -22,5 +69,16 @@ public class AnalogClock : MonoBehaviour
         secondsPivot.localRotation = Quaternion.Euler(0f, 0f, angleSeconds);
         minutesPivot.localRotation = Quaternion.Euler(0f, 0f, angleMinutes);
         hoursPivot.localRotation = Quaternion.Euler(0f, 0f, angleHours);
+
 	}
+
+    public void ChangeTimeDirectionToFoward()
+    {
+        timeRunningFoward = true;
+    }
+
+    public void ChangeTimeDirectionToReverse()
+    {
+        timeRunningFoward = false;
+    }
 }

042c4116629c3f7d8a9dd22272b3f5782b40df9f
Sonachi <red_515@hotmail.com>
2015-01-25 01:29:29 -0500

    update to gun mechanics
    almost works

---
+++ GAME JAM 2015/Assets/Prefabs/BeamBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamBehaviour : MonoBehaviour {
+
+    public bool Alive = true;
+    public float TimeTilDeath = 1.33f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (TimeTilDeath > 0)
+        {
+            TimeTilDeath -= Time.deltaTime;
+        }
+        else
+            Alive = false;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Foward")
+            Alive = false;
+    }
+}

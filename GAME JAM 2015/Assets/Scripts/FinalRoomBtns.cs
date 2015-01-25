b51e86f79af0a9315052839d52293121ef89c4a8
Stefurak <stfs0047@humbermail.ca>
2015-01-25 09:45:19 -0500

    FinalRoomScripts
    Just updating the script for the button in the final room

--- GAME JAM 2015/Assets/Scripts/FinalRoomBtns.cs
+++ GAME JAM 2015/Assets/Scripts/FinalRoomBtns.cs
@@ -4,20 +4,36 @@ using System.Collections;
 public class FinalRoomBtns : MonoBehaviour {
 
     GLOBAL_FLAGS flags;
+    GameObject spawnRm08;
+    GameObject spawnRm01;
 
     protected Animator animator;
 
 	// Use this for initialization
 	void Start () {
         flags = GameObject.Find("First Person Duck Controller").GetComponent<GLOBAL_FLAGS>();
-
+        Debug.Log(flags);
+        spawnRm01 = GameObject.Find("To Room_01");
+        Debug.Log(spawnRm01);
+        spawnRm08 = GameObject.Find("To Room_08");
+        Debug.Log(spawnRm08);
         animator = GetComponent<Animator>();
+        Debug.Log(animator);
         animator.SetBool("Pressed", false);
+        //Debug.Log(animator.GetBool);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (!flags.FINAL_ROOM_BUTTON_PRESSED)
+        {
+            Debug.Log("haventswitchspawn");
+            spawnRm08.gameObject.SetActive(true);
+            spawnRm01.gameObject.SetActive(false);
+        }else if(flags.FINAL_ROOM_BUTTON_PRESSED){
+            spawnRm08.gameObject.SetActive(true);
+            spawnRm01.gameObject.SetActive(false);
+        }
 	}
 
     void OnCollisionEnter(Collision col)
